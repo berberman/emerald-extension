aeeea6983ee4fb5c07dde930f00e325d3a25840d
berberman <1793913507@qq.com>
2018-03-29 21:43:06 +0800

    :white_check_mark: update http client util

--- src/main/kotlin/cn/berberman/emerald/util/HttpUtil.kt
+++ src/main/kotlin/cn/berberman/emerald/util/HttpUtil.kt
@@ -1,15 +1,21 @@
+import org.apache.http.Header
+import org.apache.http.HttpEntity
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.impl.client.HttpClients
 import java.net.URL
 import kotlin.coroutines.experimental.suspendCoroutine
 
 object HttpUtil {
 
+	@Deprecated("magic", ReplaceWith("get"))
 	suspend fun originGet(url: URL): String =
 			suspendCoroutine {
 				try {
 					url.openConnection().apply {
-						setRequestProperty("accept", "*/*")
-						setRequestProperty("connection", "Keep-Alive")
-						setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)")
+						setRequestProperty("Accept", "*/*")
+						setRequestProperty("Connection", "Keep-Alive")
+						setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)")
 						connect()
 					}.getInputStream().bufferedReader().readText().let(it::resume)
 				} catch (e: Exception) {
@@ -17,5 +23,35 @@ object HttpUtil {
 				}
 			}
 
+	suspend fun <R> get(uri: String, header: Header? = null, block: (result: HttpEntity) -> R): R =
+			suspendCoroutine {
+				val client = HttpClients.createDefault()
+				try {
+					HttpGet(uri).apply {
+						addHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)")
+						header?.let(this::addHeader)
+					}.let(client::execute).entity.let(block::invoke).let(it::resume)
+				} catch (e: Exception) {
+					it.resumeWithException(e)
+				} finally {
+					client.close()
+				}
+			}
+
+	suspend fun <R> post(uri: String, entity: HttpEntity? = null, header: Header? = null, block: (result: HttpEntity) -> R): R =
+			suspendCoroutine {
+				val client = HttpClients.createDefault()
+				try {
+					HttpPost(uri).apply {
+						addHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)")
+						header?.let(this::addHeader)
+						entity?.let { this.entity = it }
+					}.let(client::execute).entity.let(block::invoke).let(it::resume)
+				} catch (e: Exception) {
+					it.resumeWithException(e)
+				} finally {
+					client.close()
+				}
+			}
 
 }
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,12 +1,10 @@
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Test
-import sun.net.www.http.HttpClient
-import java.net.URL
 
 class Test {
 	@Test
 	fun test() = runBlocking {
-		HttpUtil.originGet(URL("https://api.mojang.com/users/profiles/minecraft/berberman"))
+		HttpUtil.get("https://api.mojang.com/users/profiles/minecraft/berberman") {}
 		Unit
 	}
 }
