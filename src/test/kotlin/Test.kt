9329c2d5fa2a86ad1306fd879485efe1604ad0fe
berberman <1793913507@qq.com>
2018-03-31 15:29:30 +0800

    :white_check_mark: add test

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,4 +1,7 @@
+import cn.berberman.emerald.util.onlinemode.ProfileData
+import com.google.gson.Gson
 import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Assert
 import org.junit.Test
 
 class Test {
@@ -9,5 +12,17 @@ class Test {
 		}
 		Unit
 	}
+
+	@Test
+	fun testPlayerUUID() = runBlocking {
+		val player = "berberman"
+		val map = mutableMapOf<String, String>()
+		HttpUtil.get("https://api.mojang.com/users/profiles/minecraft/$player") {
+			it.entity?.let { Gson().fromJson(it.content.bufferedReader(), ProfileData::class.java) }
+		}.let { map[player] = it?.uuid ?: "" }
+		Assert.assertEquals(player,map.entries.first().key)
+		Assert.assertEquals("b33ddc9f185e49ad8043c20593785a18",map.entries.first().value)
+	}
+
 
 }
